383db50040cc946df533bfbf041d1c258c73dc6c
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2018-10-22 21:49:24 -0300

    Creating first component

---
+++ src/components/Cards/Card.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+class Card extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Card;
