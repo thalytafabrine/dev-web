293671b5d06e1ac96ffed067fbcc67a81ebda49d
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2018-11-11 16:22:31 -0300

    Tranformando form de criação de lista em modal

--- src/components/StudyList/NewStudyList.js
+++ src/components/StudyList/NewStudyList.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import FormControl from '@material-ui/core/FormControl';
-import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const subjects = [
     {
@@ -34,54 +39,82 @@ const subjects = [
     }
 ];
 
-class TextFields extends React.Component {
-  state = {
-    name: 'Cat in the Hat',
-    currency: 'None',
-  };
+class NewStudyList extends React.Component {
+    state = {
+        name: 'Cat in the Hat',
+        currency: 'None',
+        open: false
+    };
 
-  handleChange = name => event => {
-    this.setState({
-      [name]: event.target.value,
-    });
-  };
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
+  
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+  
+    handleClose = () => {
+        this.setState({ open: false });
+    };
 
-  render() {
-
-    return (
-      <FormControl className="container" noValidate autoComplete="off" fullWidth>
-        <TextField
-          required
-          id="standard-name"
-          label="Study List Name"
-          className="textField"
-          value={this.state.name}
-          onChange={this.handleChange('name')}
-          margin="normal"
-        />
-        <TextField
-          id="standard-select-currency"
-          select
-          label="Subject"
-          className="textField"
-          value={this.state.currency}
-          onChange={this.handleChange('currency')}
-          SelectProps={{
-            MenuProps: {
-              className: "menu",
-            },
-          }}
-          margin="normal"
-        >
-          {subjects.map(option => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-      </FormControl>
-    );
-  }
+    render() {
+        return (
+        <div>
+            <Button variant="fab" color="primary" aria-label="Add" onClick={this.handleClickOpen}>
+                <AddIcon />
+            </Button>
+            <Dialog
+                open={this.state.open}
+                onClose={this.handleClose}
+                aria-labelledby="form-dialog-title"
+            >
+              <DialogTitle id="form-dialog-title">Nova Lista de Estudos</DialogTitle>
+              <DialogContent>
+                  <TextField
+                      required
+                      id="standard-name"
+                      label="Nome"
+                      className="textField"
+                      value={this.state.name}
+                      onChange={this.handleChange('name')}
+                      margin="normal"
+                  />
+                  <TextField
+                      id="standard-select-currency"
+                      select
+                      label="Disciplina"
+                      className="textField"
+                      value={this.state.currency}
+                      onChange={this.handleChange('currency')}
+                      SelectProps={{
+                          MenuProps: {
+                              className: "menu",
+                          },
+                      }}
+                      margin="normal"
+                  >
+                      {subjects.map(option => (
+                          <MenuItem key={option.value} value={option.value}>
+                              {option.label}
+                          </MenuItem>
+                      ))}
+                  </TextField>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={this.handleClose} color="primary">
+                        Cancelar
+                    </Button>
+                    <Button onClick={this.handleClose} color="primary">
+                        Criar
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+        );
+    }
 }
 
-export default TextFields;
+export default NewStudyList;
