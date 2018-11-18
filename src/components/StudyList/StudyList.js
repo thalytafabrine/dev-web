9c75d2bb1e728730d119ede919fa0139013a3aca
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2018-11-18 10:05:43 -0300

    Card de lista de estudo

---
+++ src/components/StudyList/StudyList.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Typography from '@material-ui/core/Typography';
+import { Api } from '../../services/Api';
+
+class StudyList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    delete = async() => {
+        await Api.delete(`listaEstudo/${this.props.studyList._id}`);
+        if (this.props.deleteStudyList) {
+          await this.props.deleteStudyList(this.props.studyList);
+        }
+    }
+
+    render() {
+        return(
+        <div>
+            { this.props.studyList ? (
+                <Card >
+                    <CardMedia 
+                        style={{height: 200}}
+                        image="https://www.aprenderexcel.com.br//imagens/post/385/2901-1.jpg"
+                        title={this.props.studyList.name}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h4" component="h2">
+                            {this.props.studyList.name}
+                        </Typography>
+                        <Typography component="p">
+                            {this.props.studyList.teacher}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" color="primary" href={`listaEstudo/${this.props.studyList._id}`} target="_blank">
+                            Go To Study List
+                        </Button>
+                        <IconButton onClick={this.delete} aria-label="Delete">
+                            <DeleteIcon/>
+                        </IconButton>
+                    </CardActions>
+                </Card>
+            ) : null}
+        </div>
+        )
+    }
+}
+
+export default StudyList;
