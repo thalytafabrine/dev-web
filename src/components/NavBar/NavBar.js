d03dba87c9ce0ab910107d866087a1c7bec82920
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2018-11-21 17:20:37 -0300

    Menu

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,25 +1,56 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import Typography from '@material-ui/core/Typography'
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
 
-const NavBar = () => {
-    return(
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            anchorEl: null
+        }
+    }
+
+    handleClick = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose = () => {
+        this.setState({ anchorEl: null });
+      };    
+
+    render() {
+        const { anchorEl } = this.state;
+
+        return(
         <div>
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton className="menuBottom" color="inherit" aria-label="Menu">
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="title" color="inherit">
-                Memorize
-                </Typography>
-            </Toolbar>
-        </AppBar>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton className="menuBottom" color="inherit" aria-label="Menu" onClick={this.handleClick}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={this.handleClose}
+                    >
+                        <MenuItem onClick={this.handleClose}>Disciplinas</MenuItem>
+                        <MenuItem onClick={this.handleClose}>Listas de estudo</MenuItem>
+                        <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                    </Menu>
+                    <Typography variant="title" color="inherit">
+                    Memorize
+                    </Typography>
+                </Toolbar>
+            </AppBar>
         </div>
-    )
+        )
+    }
 }
 
 export default NavBar;
